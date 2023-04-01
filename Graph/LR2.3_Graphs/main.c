579c844e2f35c49567211668565e73c7adb8d567
Dmutre <dmytro.lesko@gmail.com>
2023-04-01 13:40:15 +0200

    Not working

--- Graph/LR2.3_Graphs/main.c
+++ Graph/LR2.3_Graphs/main.c
@@ -1,8 +1,101 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
+#include <math.h>
 
-int main()
+
+//HBRUSH WHITE_BRUSH = (HBRUSH)GetStockObject(WHITE_BRUSH);
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+char ProgName[] = "Лабораторна робота 3";
+
+void arrow(float fi, int px, int py){
+    fi = 3.1416 * (180.0 - fi) / 180.0;
+    int lx, ly, rx, ry;
+    lx = px + 15 * cos(fi + 0.3);
+    rx = px + 15 * cos(fi - 0.3);
+    ly = py + 15 * sin(fi + 0.3);
+    ry = py + 15 * sin(fi - 0.3);
+    MoveToEx(hdc, px, py, NULL);
+    LineTo(hdc, px, py);
+    LineTo(hdc, rx, ry);
+    return 0;
+}
+
+void drawGraph(HWND hWnd, HDC hdc)
 {
-    printf("Hello world!\n");
+    char *nn[3] = {"1", "2", "3"};
+    int nx[3] = {100,200,300};
+    int ny[3] = {100,100,100};
+    int dx = 16, dy = 16, dtx = 5;
+    int i;
+    HPEN BPen = CreatePen(PS_SOLID, 2, RGB(50, 0, 255));
+    HPEN KPen = CreatePen(PS_SOLID, 1, RGB(20, 20, 5));
+    SelectObject(hdc, KPen);
+    MoveToEx(hdc, nx[0], ny[0], NULL);
+    LineTo(hdc, nx[1], ny[1]);
+    arrow(0,nx[1]-dx,ny[1]);
+    Arc(hdc, nx[0], ny[0]-40, nx[2], ny[2]+40, nx[2], ny[2], nx[0], ny[0]);
+    arrow(-45.0,nx[2]-dx*0.5,ny[2]-dy*0.8);
+    SelectObject(hdc, BPen);
+    for(i=0;i<=2;i++){
+        Ellipse(hdc, nx[i]-dx,ny[i]-dy,nx[i]+dx,ny[i]+dy);
+        TextOut(hdc, nx[i]-dtx,ny[i]-dy/2, nn[i],1);
+    }
+}
+
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpszCmdLine, int nCmdShow){
+    WNDCLASS w;
+
+    w.lpszClassName = ProgName;
+    w.hInstance = hInstance;
+    w.lpfnWndProc = WndProc;
+    w.hCursor = LoadCursor(NULL, IDC_ARROW);
+    w.hIcon = 0;
+    w.lpszMenuName = 0;
+    w.hbrBackground = WHITE_BRUSH;
+    w.style = CS_HREDRAW|CS_VREDRAW;
+    w.cbClsExtra = 0;
+    w.cbWndExtra = 0;
+
+    if(!RegisterClass(&w)) return 0;
+
+    HWND hWnd;
+    MSG lpMsg;
+
+    hWnd = CreateWindow(ProgName, "Лабораторна робота 3. Виконав Д. М. Лесько", WS_OVERLAPPEDWINDOW, 100, 100, 460, 240, (HWND)NULL, (HMENU)NULL, (HINSTANCE)hInstance, (HINSTANCE)NULL);
+    ShowWindow(hWnd, nCmdShow);
+    while(GetMessage(&lpMsg, hWnd, 0, 0)){
+        TranslateMessage(&lpMsg);
+        DispatchMessage(&lpMsg);
+    }
+
+    return(lpMsg.wParam);
+}
+
+LRESULT CALLBACK WndProc(HWND hWnd, UINT messg, WPARAM wParam, LPARAM lParam){
+    HDC hdc;
+    PAINTSTRUCT ps;
+    switch(messg){
+        case WM_PAINT:
+            hdc = BeginPaint(hWnd, &ps);
+            drawGraph(hWnd, hdc);
+            EndPaint(hWnd, &ps);
+            break;
+        case WM_DESTROY:
+            PostQuitMessage(0);
+            break;
+        default:
+            return DefWindowProc(hWnd, messg, wParam, lParam);
+    }
+    return 0;
+
+}
+
+
+int main() {
+
     return 0;
 }
